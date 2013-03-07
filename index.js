21c70a04dabd750dcad3076761855caf034bc1b1
Keisuke69 <nishitani.keisuke@tis.co.jp>
2013-03-07 15:04:55 +0900

    commit

--- index.js
+++ index.js
@@ -47,9 +47,10 @@ server.listen(app.get('port'), function(){
 io.sockets.on('connection', function (socket) { //コネクション接続時のイベント
   console.log("connected");// コネクション接続時の処理を記述
   socket.on('msg', function(data){
-    var message = data;
+    var date = data.date;
+    var message = data.message;
     console.log(data);
-    io.sockets.emit("hoge", message);
+    io.sockets.emit("msg", {date : date, message : message});
   });
   //socket.emit('welcome', { hello: 'world' }
  });
